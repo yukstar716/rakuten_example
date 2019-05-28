16c3894b0c860cca2703e3d5b87a77e406ffc665
Yuki Matsumoto <yumatsu.july19920716@gmail.com>
2019-05-28 09:15:48 +0900

    edit views

--- config/initializers/rakuten.rb
+++ config/initializers/rakuten.rb
@@ -1,11 +1,7 @@
-RakutenWebService.configuration do |c|
-
+RakutenWebService.configure do |c|
   c.application_id = '1098958799307606422'
-
   c.affiliate_id = '165d1a29.8d94c3de.165d1a2a.674be0c1'
-
   c.max_retries = 3
-
   c.debug = true
-
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  get 'rakuten_search' => 'rakuten#search'
+  get 'rakuten_search', to: 'rakuten#search'
 end
